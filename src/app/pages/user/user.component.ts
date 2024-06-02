0e216e47956042aaf60f3c1f45a365dfa4334da3
pratiksha <pratikshajankar176@gmail.com>
2024-06-02 08:46:43 +0530

    user completed

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../../core/models/interfaces/IUser';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../../core/services/user/user.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { User } from '../../core/models/classes/User';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+
+  userList:IUser[]=[];
+  userobj:User=new User();
+
+  constructor(private http:HttpClient,private usersrv:UserService){
+
+  }
+
+  ngOnInit(): void {
+    this.getAllUser();
+
+  }
+
+  getAllUser(){
+    this.usersrv.getAllUser().subscribe((res:any)=>{
+      this.userList=res.data;
+    })
+  }
+
+  onEdit(id:any){
+this.userobj=id;
+  }
+
+  onUpdate(){
+    this.usersrv.UpdateUser(this.userobj).subscribe((res:any)=>{
+      if(res.result){
+        alert("data updated successfully");
+        this.getAllUser();
+      }
+      else{
+        alert(res.message);
+      }
+    })
+  }
+
+  onDelete(uid:any){
+this.usersrv.DeleteUser(uid.userId).subscribe((res:any)=>{
+if(res.result){
+  alert("data deleted successfully");
+  this.getAllUser();
+}
+else{
+  alert(res.message);
+}
+})
+  }
+
+  onreset(){
+this.userobj=new User();
+  }
 
 }
